3424c7377fe61b433968d98b8c6e12a46d73aa2f
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-08-31 23:43:10 +0300

    Mission Passed

---
+++ lib/insert.sql
@@ -0,0 +1,18 @@
+-- Mr. Chocolate
+-- Rowdy
+-- Tabitha
+-- Sergeant Brown
+-- Melissa
+-- Grinch
+-- Wendy
+-- unnamed (refer back to how to create a record that doesn't have one value)
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES(1, "Mr.Chocolate", 7, "M", "Dark Brown","Shy", True);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES(2, "Melissa", 4, "F", "Black","Outgoing", False);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES(3, "Rowdy", 9, "M", "White","Shy", True);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES(4, "Mr.Chocolate", 6, "F", "Dark Blue","Outgoing", False);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES(5, "Sergeant Brown", 10, "M", "Light Brown","Shy", False);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES(6, "Tabitha", 3, "F", "Red","Outgoing", True);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES(7, "Grinch", 2, "M", "Black and White","Shy", True);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES(8, "Wendy", 11, "F", "White and Brown","Outgoing", False);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES(9, NULL, 1, "M", "Red and White","Shy", True);
